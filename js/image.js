5c76265ccd705191cc895552ac5864a201fc007b
YAUHENI FIARBEI <128324742+yafiarbe@users.noreply.github.com>
2024-01-16 12:19:06 +0100

    add files

---
+++ js/image.js
@@ -0,0 +1,52 @@
+let cardsTexts = document.querySelectorAll(".card");
+// Перебираем все карточки
+for (let card of cardsTexts) {
+    let textscard = card.querySelectorAll(".card *");
+    // Перебираем все элементы text
+    for (let text of textscard) {
+        // Получаем содержимое элемента text
+        let content = text.innerHTML;
+        // Создаем новый элемент img с атрибутом src
+        let img = document.createElement("img");
+        img.src = "./svg/1inch.svg";
+        // Заменяем все вхождения слова "2inch" на элемент img в содержимом с помощью регулярного выражения
+        content = content.replace(/--1--/g, img.outerHTML);
+        // Обновляем содержимое элемента text
+        text.innerHTML = content;
+    }
+    for (let text of textscard) {
+        // Получаем содержимое элемента text
+        let content = text.innerHTML;
+        // Создаем новый элемент img с атрибутом src
+        let img = document.createElement("img");
+        img.src = "./svg/2inch.svg";
+        // Заменяем все вхождения слова "2inch" на элемент img в содержимом с помощью регулярного выражения
+        content = content.replace(/--2--/g, img.outerHTML);
+        // Обновляем содержимое элемента text
+        text.innerHTML = content;
+    }
+    for (let text of textscard) {
+        // Получаем содержимое элемента text
+        let content = text.innerHTML;
+        // Создаем новый элемент img с атрибутом src
+        let img = document.createElement("img");
+        img.src = "./svg/3inch.svg";
+        // Заменяем все вхождения слова "2inch" на элемент img в содержимом с помощью регулярного выражения
+        content = content.replace(/--3--/g, img.outerHTML);
+        // Обновляем содержимое элемента text
+        text.innerHTML = content;
+    }
+    for (let text of textscard) {
+        // Получаем содержимое элемента text
+        let content = text.innerHTML;
+        // Создаем новый элемент img с атрибутом src
+        let img = document.createElement("img");
+        img.src = "./svg/6inch.svg";
+        // Заменяем все вхождения слова "2inch" на элемент img в содержимом с помощью регулярного выражения
+        content = content.replace(/--6--/g, img.outerHTML);
+        // Обновляем содержимое элемента text
+        text.innerHTML = content;
+    }
+}
+
+
